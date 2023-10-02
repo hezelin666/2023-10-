a3150175a9426b29ef6c64e61b9d82d3b7b5249d
86158 <86158@HZL>
2023-10-02 20:22:03 +0800

    添加项目文件。

---
+++ 100201.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	int i;
+
+	for (i = 0; i < 10;i++)
+	{
+		printf("%d\n", a[i]);
+
+	}
+	printf("%d\n", sizeof(a));
+	return 0;
+}
